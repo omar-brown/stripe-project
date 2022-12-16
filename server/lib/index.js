634af1aa75d8cce00d028fd681ea3ee159d90660
Maher Brown <brown.maher42@gmail.com>
2022-12-15 19:42:09 -0500

    added imports

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,3 +1,19 @@
 "use strict";
-console.log('test');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.stripe = void 0;
+const dotenv_1 = require("dotenv");
+if (process.env.NODE_ENV !== 'production') {
+    (0, dotenv_1.config)();
+}
+const stripe_1 = __importDefault(require("stripe"));
+exports.stripe = new stripe_1.default(process.env.STRIPE_SECRET, {
+    //@ts-ignore
+    apiVersion: '2020-03-02'
+});
+const api_1 = require("./api");
+const port = process.env.PORT || 3333;
+api_1.app.listen(port, () => console.log(`API is available on http://localhost:${port}`));
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('test');
+import { config } from "dotenv";
+
+if(process.env.NODE_ENV !== 'production'){
+    config();
+}
+
+import Stripe from "stripe";
+export const stripe = new Stripe(process.env.STRIPE_SECRET, {
+    //@ts-ignore
+    apiVersion: '2020-03-02'
+})
+
+import { app } from './api'
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`API is available on http://localhost:${port}`));
