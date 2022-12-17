ce454792aece278d15f6ff30ae6f6d37102929a2
Maher Brown <brown.maher42@gmail.com>
2022-12-17 15:14:55 -0500

    added test api setup

--- server/lib/api.js
+++ server/lib/api.js
@@ -6,4 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 exports.app = (0, express_1.default)();
+// Middleware
+exports.app.use(express_1.default.json());
+const cors_1 = __importDefault(require("cors"));
+exports.app.use((0, cors_1.default)({ origin: true }));
+exports.app.post('/test', (req, res) => {
+    const amount = req.body.amount;
+    res.status(200).send({ with_tax: amount * 7 });
+});
 //# sourceMappingURL=api.js.map
--- server/src/api.ts
+++ server/src/api.ts
@@ -1,2 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 export const app = express();
+
+// Middleware
+app.use(express.json());
+
+import cors from 'cors';
+app.use(cors({origin: true}));
+
+
+app.post('/test', (req: Request, res: Response) => {
+    const amount = req.body.amount;
+    res.status(200).send({ with_tax: amount * 7 })
+})
