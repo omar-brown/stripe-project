be1427faad35ed61a22ff104308c20003e351f1d
Maher Brown <brown.maher42@gmail.com>
2022-12-17 15:59:47 -0500

    Added checkout and adjusted .gitignore

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,14 +1,36 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 export const app = express();
 
 // Middleware
 app.use(express.json());
 
 import cors from 'cors';
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 
-app.post('/test', (req: Request, res: Response) => {
-    const amount = req.body.amount;
-    res.status(200).send({ with_tax: amount * 7 })
-})
+/**
+ * Handle api endpoint for checkout
+ */
+import { createStripeCheckoutSession } from './checkout';
+
+app.post(
+    '/checkouts', runAsync(async ({ body }: Request, res: Response) => {
+        res.send(
+            //Await result of helper function, pass line items from body
+            await createStripeCheckoutSession(body.line_items)
+        )
+    })
+)
+
+/**
+ * Catch async errors when awaiting promises
+ * Express doesn't normally handle async errors, and will just cause the UI to hang
+ * This function will help handle errors more gracefully
+ */
+
+function runAsync(callback: Function) {
+    console.log(callback)
+    return (req: Request, res: Response, next: NextFunction) => {
+        callback(req, res, next).catch(next)
+    }
+}
