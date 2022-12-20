ae7d07f34f02ebdc6e24287c8db5789a8583cd2c
Maher Brown <brown.maher42@gmail.com>
2022-12-20 15:17:25 -0500

    Added a way to get or create customers

---
+++ server/src/customers.ts
@@ -0,0 +1,30 @@
+import { stripe } from "./";;
+import { db } from "./firebase";
+import Stripe from "stripe";
+
+/**
+ * Gets the existing Stripe customer or creates a new record
+ */
+export async function getOrCreateCustomer(userId: string, params?: Stripe.CustomerCreateParams) {
+    // Pull the firestore document and check for a stripe customer id
+    const userSnapshot = await db.collection('users').doc(userId).get();
+    const { stripeCustomerId, email } = userSnapshot.data();
+
+    // If missing customerID, create it
+    if (!stripeCustomerId) {
+        // Include the firebase uid in the customer's metadata to link everything up
+        const customer = await stripe.customers.create({
+            email,
+            metadata: {
+                firebaseUID: userId
+            },
+            ...params
+        });
+        // Update firestore with stripeCustomerId
+        await userSnapshot.ref.update({ stripeCustomerId: customer.id });
+        return customer;
+    } else {
+        // If the customer does exist, retrieve it from stripe and return it
+        return await stripe.customers.retrieve(stripeCustomerId) as Stripe.Customer
+    }
+}
