9766a0a867e51ebb34d31a5ab31f876280a23ee6
Maher Brown <brown.maher42@gmail.com>
2022-12-23 21:38:47 -0500

    changed url

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -1,6 +1,6 @@
 import { auth } from "./firebase";
 
-const API = 'https://server-sucaufqy2a-uc.a.run.app'
+const API = 'https://server-sfnuosnn4a-uc.a.run.app'
 
 /**
  * Function to fetch data from your API
